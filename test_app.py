99a2a4a087925847f56766ba49fe7779c07ef4fa
eskayML <kalusam619@gmail.com>
2023-04-30 18:53:00 +0100

    formatted with black

--- test_app.py
+++ test_app.py
@@ -1,20 +1,16 @@
 from app import app, COLUMNS
 import pytest
 
+
 def test_index():
     with app.test_client() as client:
-        response = client.get('/')
-
+        response = client.get("/")
         assert response.status_code == 200
-
-
 
 
 def test_predict_page():
-    dummy_values = [1]*10
+    dummy_values = [1] * 10
     with app.test_client() as client:
-        response = client.post('/predict', data = dict(zip(COLUMNS, dummy_values)))
+        response = client.post("/predict", data=dict(zip(COLUMNS, dummy_values)))
         assert response.status_code == 200
-
-        assert  b'The model predicted' in response.data
-        
+        assert b"The model predicted" in response.data
