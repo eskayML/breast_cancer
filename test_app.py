f8d80bfc13318b95022a1b3607b4813b80856665
eskayML <kalusam619@gmail.com>
2023-05-01 19:12:57 +0100

    add: confusion matrix

--- test_app.py
+++ test_app.py
@@ -8,9 +8,14 @@ def test_index():
         assert response.status_code == 200
 
 
-def test_predict_page():
+def test_predict():
+    with app.test_client() as client:
+        response = client.get("/predict")
+        assert response.status_code == 200
+
+
+def test_submit_form():
     dummy_values = [1] * 10
     with app.test_client() as client:
         response = client.post("/predict", data=dict(zip(COLUMNS, dummy_values)))
-        assert response.status_code == 200
         assert b"The model predicted" in response.data
